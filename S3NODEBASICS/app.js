fb3bfdcd1642bb046af28f7e30a72351862bcd1a
Alun Price <alunprice089@gmail.com>
2022-11-21 12:40:00 +0000

    s3 36 Done writeFile

--- S3NODEBASICS/app.js
+++ S3NODEBASICS/app.js
@@ -27,11 +27,12 @@ const server = http.createServer((req, res) => {
     req.on('end', () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
   }
 
   res.setHeader('Content-Type', 'text/html');
