1a6d24cb443ce612ceb6c0f716a8dde9cb3bd452
Alun Price <alunprice089@gmail.com>
2022-11-22 07:11:46 +0000

    s5 63 Add some route handling

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware');
-  next(); // Allows the request to continue to the next middleware in line
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h1>The Add-Product Page</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log('In another middleware');
   res.send('<h1>Hello from Express!</h1>');
 });
