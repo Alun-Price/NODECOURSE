a7d32674ac8d00cc03eb2f4d8ffb35fdb9fe904f
Alun Price <alunprice089@gmail.com>
2022-11-22 08:14:28 +0000

    s5 66 Add admin and shop routes

--- app.js
+++ app.js
@@ -3,21 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoutes = require('./routes/admin.js');
+const shopRoutes = require('./routes/shop.js');
+
 app.use(bodyParser.urlencoded());
 
-app.post('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
