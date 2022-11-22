26feaf61539569f8cd50fb220c52b9ab95bb4d76
Alun Price <alunprice089@gmail.com>
2022-11-22 00:02:04 +0000

    Section 3 Understanding the Basics Done

--- S3NODEBASICS/app.js
+++ S3NODEBASICS/app.js
@@ -1,47 +1,6 @@
 const http = require('http');
-const fs = require('fs');
-// import { Http2ServerResponse } from 'http2';
+const routes = require('./routes.js');
 
-// http.createServer(function (req, res) {});
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  const method = req.method;
-
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></input></form></body>'
-    );
-    res.write('</html>');
-    return res.end();
-  }
-
-  if (url === '/message' && method === 'POST') {
-    const body = [];
-    req.on('data', chunk => {
-      //console.log('CHUNK: ', chunk);
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFile('message.txt', message, err => {
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js server</h1></body>');
-  res.write('</html>');
-  res.end();
-  //   process.exit();
-});
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
