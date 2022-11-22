a5a9f6878485aa3a24963866e0ddb3db782916e8
Alun Price <alunprice089@gmail.com>
2022-11-22 10:13:06 +0000

    s5 70 res.sendFile for serving HTML pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
 // /admin/add-products => GET
 router.get('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
 // /admin/products => POST
