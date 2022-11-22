c8beabc62f8a00559b2d49f2c358dec7becba5d1
Alun Price <alunprice089@gmail.com>
2022-11-22 08:58:32 +0000

    s5 67 add a 404 page

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.use(bodyParser.urlencoded());
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page Not Found!</h1>');
+});
+
 app.listen(3000);
