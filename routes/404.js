f338000338460a112771f55fbe362cc23812fd36
Alun Price <alunprice089@gmail.com>
2022-11-24 23:59:36 +0000

    s5 73 add a rootDir helper function for navigation

--- routes/404.js
+++ routes/404.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path.js');
+
 const router = express.Router();
 
 router.use('/', (req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, '../', 'views', '404.html'));
+  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
 });
 
 module.exports = router;
--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path.js');
+
 const router = express.Router();
 
 // /admin/add-products => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/products => POST
