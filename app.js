ee5000eef910211ab8b5e130fb361d87bb3b1bf6
Alun Price <alunprice089@gmail.com>
2022-11-27 00:39:09 +0000

    install templating engines (pug, ejs and handlebars), register pug and add shop.pug

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 const adminData = require('./routes/admin.js');
 const shopRoutes = require('./routes/shop.js');
 const fourOhFourRoutes = require('./routes/404.js');
