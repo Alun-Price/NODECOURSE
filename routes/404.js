885d3ed28d6ea520dc7a8c45f3fbe19a9431fcfd
Alun Price <alunprice089@gmail.com>
2022-11-29 18:00:08 +0000

    s6 84 convert html files to pug

--- routes/404.js
+++ routes/404.js
@@ -7,7 +7,7 @@ const rootDir = require('../util/path.js');
 const router = express.Router();
 
 router.use('/', (req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: '404 Not Found' });
 });
 
 module.exports = router;
--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,8 @@ const products = [];
 
 // /admin/add-products => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', { pageTitle: 'Add Product' });
 });
 
 // /admin/products => POST
