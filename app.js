54dd289f4defb5dd18936fd50250e2bd76191d8e
Alun Price <alunprice089@gmail.com>
2022-12-12 16:51:25 +0000

    s10 140 connecting the app to the mysql db

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const db = require('./util/database');
 const app = express();
 
 app.set('view engine', 'ejs');
