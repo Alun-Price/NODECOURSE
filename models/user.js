5859ebecaecfacb135f5edd0fedbd9de7d7b2ab3
Alun Price <alunprice089@gmail.com>
2022-12-19 16:30:18 +0000

    s11 161 Add a User model

---
+++ models/user.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  name: Sequelize.STRING,
+  email: {
+    type: Sequelize.email,
+    allowNull: false,
+  },
+});
+
+module.exports = User;
