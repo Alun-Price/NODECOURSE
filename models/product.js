a40b22d6fbfe5f0faa7b7b0f3b93826366f16d69
Alun Price <alunprice089@gmail.com>
2022-12-19 11:33:27 +0000

    s10 end 148 inserting products and seeing the details on the product detail page

--- models/product.js
+++ models/product.js
@@ -2,15 +2,20 @@ const db = require('../util/database');
 
 const Cart = require('./cart');
 module.exports = class Product {
-  constructor(id, title, imageUrl, price, description) {
+  constructor(id, title, price, description, imageUrl) {
     this.id = id;
     this.title = title;
-    this.imageUrl = imageUrl;
     this.price = price;
     this.description = description;
+    this.imageUrl = imageUrl;
   }
 
-  save() {}
+  save() {
+    return db.execute(
+      'INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
+      [this.title, this.price, this.description, this.imageUrl]
+    );
+  }
 
   static deleteById(id) {}
 
@@ -18,5 +23,7 @@ module.exports = class Product {
     return db.execute('SELECT * FROM products');
   }
 
-  static findById(id) {}
+  static findById(id) {
+    return db.execute('SELECT * FROM products WHERE id = ?', [id]);
+  }
 };
