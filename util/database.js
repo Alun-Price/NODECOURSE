b6e669756b37dc6b13f43075041e01b5d2894685
Alun Price <alunprice089@gmail.com>
2022-12-19 12:02:14 +0000

    s11 151 install sequelize and use it to connect to the database

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', 'Effect!ve333', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'Effect!ve333',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
