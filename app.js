97786e7b837b7b1f778880574c7285d28208eed4
Alun Price <alunprice089@gmail.com>
2022-12-20 01:07:09 +0000

    add CartItem model, rewrite Cart model, establish new table relationships

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const app = express();
 
@@ -34,8 +36,13 @@ app.use(fourOhFourRoutes);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
+  //.sync({ force: true })
   .sync()
   .then(result => {
     return User.findByPk(1);
